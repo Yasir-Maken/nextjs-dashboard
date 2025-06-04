77f5946ab02eaf60ef0a0bf0a587c18795af987f
Yasir-Maken <yasir.maken.cs24@gmail.com>
2025-06-04 22:08:29 +0300

    NextJs Dashboard: Practicing NextJs from Official Site a very nice Training

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+
+
+export default function page() {
+    return (
+        <p>Dashbaord Page</p>
+    );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+
+import { Inter } from 'next/font/google'; // Import and download Inter fonts from google to my project
+export const inter = Inter({ subsets: ['latin'] });
+
+
+
+import { Lusitana } from 'next/font/google'; // Import and download Lusitana fonts to my project
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
